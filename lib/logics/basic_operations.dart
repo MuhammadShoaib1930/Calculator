4dc4261d2997d2627cf43ddcf24844062b8d41e2
Muhammad Shoaib <shoiabsn54321@gmail.com>
2024-10-20 15:31:19 +0500

    update feature

--- lib/logics/basic_operations.dart
+++ lib/logics/basic_operations.dart
@@ -1,71 +1,56 @@
-// import 'package:calculator/screens_pages/home_page.dart';
+import 'package:calculator/screens_pages/home_page.dart';
 
-// String inputValues = "";
+List<String> listFromString(String input) {
+  RegExp regex = RegExp(r'([+\-\*\/]|\d+\.\d+|\d+)');
+  List<String> result =
+      regex.allMatches(input).map((match) => match.group(0)!).toList();
+  if (result[0] == "-") {
+    result[0] = "-${result[1]}";
+    result.removeAt(1);
+  }
+  return result;
+}
 
-// buttonsFunctional(String label) {
-//   if (label == "C") {
-//     inputValues = "";
-//   } else if (label == "_" && inputValues.isNotEmpty) {
-//     inputValues = inputValues.substring(0, inputValues.length - 1);
-//   } else if (label == "=") {
-//     mainFunction(isTrue: true);
-//     streamControllerList.add(history);
-//   } else if (label != "_") {
-//     inputValues += label;
-//   }
-//   textController.text = inputValues;
-// }
+String calculateResult(List<String> list) {
+  for (int i = 0; i < list.length; i++) {
+    if (list[i] == '*' || list[i] == '/') {
+      double left = double.parse(list[i - 1].toString());
+      double right = double.parse(list[i + 1].toString());
+      double result;
+      if (list[i] == '*') {
+        result = left * right;
+      } else {
+        result = left / right;
+      }
+      list[i - 1] = result.toString();
+      list.removeRange(i, i + 2);
+      i--;
+    }
+  }
+  for (int i = 0; i < list.length; i++) {
+    if (list[i] == '+' || list[i] == '-') {
+      double left = double.parse(list[i - 1].toString());
+      double right = double.parse(list[i + 1].toString());
+      double result;
+      if (list[i] == '+') {
+        result = left + right;
+      } else {
+        result = left - right;
+      }
+      list[i - 1] = result.toString();
+      list.removeRange(i, i + 2);
+      i--;
+    }
+  }
+  return list[0].toString();
+}
 
-// List<String> stringToList(String input) {
-//   RegExp regex = RegExp(r'([+\-\*\/]|\d+\.\d+|\d+)');
-//   List<String> result =
-//       regex.allMatches(input).map((match) => match.group(0)!).toList();
-//   if (result[0] == "-") {
-//     result[0] = "-${result[1]}";
-//     result.removeAt(1);
-//   }
-//   return result;
-// }
+String calculationFunction(String input) {
+  String temp = calculateResult(listFromString(input));
+  streamController.add(temp);
+  return temp;
+}
 
-// String calculateResult(List<String> list) {
-//   for (int i = 0; i < list.length; i++) {
-//     if (list[i] == '*' || list[i] == '/') {
-//       double left = double.parse(list[i - 1].toString());
-//       double right = double.parse(list[i + 1].toString());
-//       double result;
-//       if (list[i] == '*') {
-//         result = left * right;
-//       } else {
-//         result = left / right;
-//       }
-//       list[i - 1] = result.toString();
-//       list.removeRange(i, i + 2);
-//       i--;
-//     }
-//   }
-//   for (int i = 0; i < list.length; i++) {
-//     if (list[i] == '+' || list[i] == '-') {
-//       double left = double.parse(list[i - 1].toString());
-//       double right = double.parse(list[i + 1].toString());
-//       double result;
-//       if (list[i] == '+') {
-//         result = left + right;
-//       } else {
-//         result = left - right;
-//       }
-//       list[i - 1] = result.toString();
-//       list.removeRange(i, i + 2);
-//       i--;
-//     }
-//   }
-//   return list[0].toString();
-// }
-
-// mainFunction({bool isTrue = false}) {
-//   var temp = calculateResult(stringToList(inputValues)).toString();
-//   streamControllerString.add(temp);
-//   history.add(temp);
-//   if (isTrue) {
-//     inputValues = temp.toString();
-//   }
-// }
+buttonClicked() {
+  inputsValue = "";
+}
--- lib/screens_pages/home_page.dart
+++ lib/screens_pages/home_page.dart
@@ -1,5 +1,6 @@
-import 'dart:async';
 
+import 'dart:async';
+import 'package:calculator/logics/basic_operations.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -10,9 +11,10 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+StreamController streamController = StreamController();
+
 class _HomePageState extends State<HomePage> {
-  StreamController streamController = StreamController();
-  List<String> list = ['1','2','3'];
+  List<String> list = [];
   TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -24,7 +26,7 @@ class _HomePageState extends State<HomePage> {
         body: Column(children: [
           SizedBox(
             width: double.infinity,
-            height: 310,
+            height: 260,
             child: StreamBuilder(
               initialData: "WellCome to Calculator",
               stream: streamController.stream,
@@ -38,11 +40,8 @@ class _HomePageState extends State<HomePage> {
                               list[index].toString(),
                               textAlign: TextAlign.right,
                               style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w300
-                              ),
-                              
-                              ),
+                                  fontSize: 18, fontWeight: FontWeight.w300),
+                            ),
                           );
                         },
                         itemCount: list.length,
@@ -52,12 +51,26 @@ class _HomePageState extends State<HomePage> {
               },
             ),
           ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                list.clear();
+                textEditingController.value = TextEditingValue.empty;
+                streamController.add(0);
+              },
+              child: const Text("Clean")),
+          const SizedBox(
+            height: 10,
+          ),
           Form(
               child: SizedBox(
             width: 380,
             child: TextFormField(
               controller: textEditingController,
-              textAlign:TextAlign.end,
+              textAlign: TextAlign.end,
+              keyboardType: TextInputType.none,
               showCursor: true,
               style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               decoration: const InputDecoration(
@@ -65,18 +78,96 @@ class _HomePageState extends State<HomePage> {
                       borderRadius: BorderRadius.all(Radius.circular(20)))),
               autovalidateMode: AutovalidateMode.onUserInteraction,
               validator: (value) {
+                bool isEqual = false;
                 if (value == null || value.isEmpty) {
                   return null;
                 }
                 value = value.trim();
-                if (!RegExp(r'^-?(\d+(\.\d+)?)([+\-*/](\d+(\.\d+)?))*$').hasMatch(value)) {
-                  return 'Invalid expression. Only valid numbers and operators are allowed';
+                if (!RegExp(r'^-?(\d+(\.\d+)?)([+\-*/](\d+(\.\d+)?))*$')
+                    .hasMatch(value)) {
+                  if (RegExp(r'=$').hasMatch(value)) {
+                    isEqual = true;
+                  } else {
+                    return 'Invalid expression.';
+                  }
                 }
-                // valid 
+                // valid
+                var inputsValue = textEditingController.text.toString();
+                var outputValues = calculationFunction(inputsValue);
+                textEditingController.text = outputValues;
+                if (isEqual) {
+                  list.add(outputValues);
+                  if (list.length > 10) {
+                    list.removeAt(0);
+                  }
+                }
+
                 return null;
               },
             ),
-          ))
+          )),
+          KeyBoardLayout(textEditingController: textEditingController)
         ]));
   }
+}
+
+class KeyBoardLayout extends StatelessWidget {
+  final TextEditingController textEditingController;
+  const KeyBoardLayout({super.key , required this.textEditingController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              elevatedButtonFunction("+", textEditingController),
+              elevatedButtonFunction("1", textEditingController),
+              elevatedButtonFunction("2", textEditingController),
+              elevatedButtonFunction("3", textEditingController),
+              elevatedButtonFunction("%", textEditingController),
+            ],
+          ),
+          Row(
+            children: [
+              elevatedButtonFunction("-", textEditingController),
+              elevatedButtonFunction("4", textEditingController),
+              elevatedButtonFunction("5", textEditingController),
+              elevatedButtonFunction("6", textEditingController),
+              elevatedButtonFunction("_", textEditingController),
+            ],
+          ),
+          Row(
+            children: [
+              elevatedButtonFunction("/", textEditingController),
+              elevatedButtonFunction("7", textEditingController),
+              elevatedButtonFunction("8", textEditingController),
+              elevatedButtonFunction("9", textEditingController),
+              elevatedButtonFunction(".", textEditingController),
+            ],
+          ),
+          Row(
+            children: [
+              elevatedButtonFunction("*", textEditingController),
+              elevatedButtonFunction("0", textEditingController),
+              elevatedButtonFunction("py", textEditingController),
+              elevatedButtonFunction("SqR", textEditingController),
+              elevatedButtonFunction("=", textEditingController),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+elevatedButtonFunction(
+    String clickValue, TextEditingController textEditingController) {
+  return ElevatedButton(
+      onPressed: () {
+        textEditingController.text = clickValue;
+      },
+      child: Text(clickValue));
 }
